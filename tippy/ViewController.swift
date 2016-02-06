52dbcca7acd6de7906510389c9f0dbaa69a8542a
Ron Lai <ron.lai@invoice2go.com>
2016-02-05 16:26:08 -0800

    first commit

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billText: UITextField!
+    @IBOutlet weak var tipSelect: UISegmentedControl!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tipLabel.text = "-";
+        totalLabel.text = "-";
+    }
+    override func viewDidAppear(animated: Bool) {
+        // auto select billText
+        billText.becomeFirstResponder();
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChange(sender: AnyObject) {
+        var billAmount = billText.text._bridgeToObjectiveC().doubleValue
+        var tip = billAmount * [0.18, 0.20, 0.22][tipSelect.selectedSegmentIndex];
+        var total = billAmount + tip;
+        
+        var formatter = NSNumberFormatter();
+        formatter.numberStyle = .CurrencyStyle;
+        formatter.maximumFractionDigits = 2;
+        
+        tipLabel.text = formatter.stringFromNumber(tip);
+        totalLabel.text = formatter.stringFromNumber(total);
+    }
 }
 
